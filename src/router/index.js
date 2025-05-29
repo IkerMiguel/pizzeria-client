1d58fa927bd62db183b2dfa7e1b458d376498c10
IkerMiguel <ikmiguel03@gmail.com>
2025-05-28 23:54:37 -0500

    Add Index Ingredient

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,7 @@ import NewPizzaRawMaterial from '../components/Pizza_raw_material/NewPizzaRawMat
 import Order_extra_ingredients from '../views/Order_extra_ingredients.vue'
 import EditOrderExtraIngredient from '../components/Order_extra_ingredient/EditOrderExtraIngredient.vue'
 import NewOrderExtraIngredient from '../components/Order_extra_ingredient/NewOrderExtraIngredient.vue'
+import Ingredient from '@/views/Ingredient.vue'
 
 const routes = [
   {
@@ -127,6 +128,11 @@ const routes = [
   path: '/add-pedido-ingrediente-extra',
   name: 'NewOrderExtraIngredient',
   component: NewOrderExtraIngredient
+},
+{
+  path: '/Ingredients',
+  name: 'Ingredient',
+  component: Ingredient
 }
 
 ]
